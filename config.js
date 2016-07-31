37a2193baed6672303e76b53458f3f35d51beed5
karim <karimabdelhakim22@hotmail.com>
2016-08-01 00:07:23 +0200

    mongo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : 'mongodb://karim:123456@ds139715.mlab.com:39715/confusionwebapp',//'mongodb://localhost:27017/conFusion',
     'facebook': {
         clientID: '1757698561154750',
         clientSecret: 'db93d73d5da3eceb02744c0bc0d51b6e',
