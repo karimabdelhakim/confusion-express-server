c985af2d214cfd07f5c3df645dd5042d085a30a4
karim abdelhakim <karimabdelhakim22@hotmail.com>
2016-11-23 04:16:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'mongoLab' : 'mongodb://karim:123456@ds139715.mlab.com:39715/confusionwebapp',
+    'mongoLab' : 'mongodb://username:pass@ds139715.mlab.com:39715/confusionwebapp',
     'mongoEmul' : 'mongodb://192.168.1.5:27017/conFusion',
     'facebook': {
         clientID: '1757698561154750',
